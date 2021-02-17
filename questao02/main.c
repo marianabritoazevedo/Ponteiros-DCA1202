780a731a89c026409de15327d45aea3d64162cc4
Mariana Brito Azevedo <69637198+marianabritoazevedo@users.noreply.github.com>
2021-02-17 18:23:38 -0300

    Q2Atualizada

--- questao02/main.c
+++ questao02/main.c
@@ -6,10 +6,11 @@ int main()
     int i=5, *p; // i ocupa o endereço 4094 na memória
     p = &i; // p guarda o ENDEREÇO de i
     printf("%x %d %d %d %d", p,*p+2,**&p,3**p,**&p+4);
-    // p = 4094, que é o endereço da variável i 
+    // p = 4094, que é o endereço em hexadecimal da variável i 
     // *p (conteúdo do endereço armazenado em p) + 2 = 5 + 2 = 7 
-    // **&p (conteúdo do endereço armazenado em p) = 5 
+    // **&p (conteúdo do conteúdo do endereço armazenado em p) = 5 
     //3**p = 3 * 5 = 15, sendo 5 o conteúdo do endereço armazenado em p
-    // **&p + 4 = 5 + 4 = 9
+    // **&p (conteúdo do conteúdo do endereço armazenado em p) + 4 = 5 + 4 = 9
     // o que será impresso: 4094 7 5 15 9
+    return 0;
 }
