d667a285b783534b7b5f11ecdb3f2b6fe2d05358
Mariana Brito Azevedo <69637198+marianabritoazevedo@users.noreply.github.com>
2021-02-17 18:20:56 -0300

    Update main.c

--- questao01/main.c
+++ questao01/main.c
@@ -12,11 +12,10 @@ int main()
     q = &j; //guardando o endereço de j em q
     p == &i; // comparando se p é igual ao endereço de i, e isto retorna o valor True
     *p - *q; // conteúdo de p - conteúdo de q = 3 - 5 = -2
-    **&p; // Irá retornar o valor 3, que é o conteúdo armazenado no ponteiro p. Entende-se que um * irá se "cancelar" com um &
-    printf("%d",**&p);
-    printf ("\n");
+    **&p; // Irá retornar o valor 3, que é o conteúdo armazenado no ponteiro p. Entende-se que  ele irá retornar o conteúdo do conteúdo do endereço de p, que é o próprio
+    printf("%d",**&p); //conferindo resposta acima
     3 - *p/(*q) + 7; // 3 - 3/5 + 7 = 3 - 0 + 7 = 10
-    printf("%d ", 3 - *p/(*q) + 7);
+    printf("%d ", 3 - *p/(*q) + 7); //conferindo resposta acima
     return 0;
 
 }
